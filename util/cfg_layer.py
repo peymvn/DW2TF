2c878d57058d84d76f6a96ea153a58c898400a10
Peyman Ghasemi <p_gmtz@yahoo.com>
2020-05-12 15:52:14 +0430

    print(param)

--- util/cfg_layer.py
+++ util/cfg_layer.py
@@ -159,7 +159,7 @@ def cfg_yolo(B, H, W, C, net, param, weights_walker, stack, output_index, scope,
 
 def cfg_upsample(B, H, W, C, net, param, weights_walker, stack, output_index, scope, training, const_inits, verbose):
     stride = int(param['stride'])
-    print(stride)
+    print(param)
     assert stride == 2
 
     net = tf.image.resize_nearest_neighbor(net, (H * stride, W * stride), name=scope)
